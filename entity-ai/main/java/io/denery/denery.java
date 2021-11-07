cf504bbc9734741645d7de8bb43e27915b74b6b3
Denery <63610262+ExtraExtremeERA@users.noreply.github.com>
2021-11-07 19:02:49 +0300

    initial

---
+++ entity-ai/main/java/io/denery/denery.java
@@ -0,0 +1,16 @@
+package io.denery;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class denery extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
