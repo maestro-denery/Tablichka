a3fecb1cb3664ed197c741c098e4f9b3c8baed15
Denery <63610262+ExtraExtremeERA@users.noreply.github.com>
2021-11-08 02:00:30 +0300

    Fully Working Architecure

---
+++ EntityAI/main/java/org/tablichka/AI.java
@@ -0,0 +1,7 @@
+package org.tablichka;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class AI {
+
+}
--- Launcher/main/java/org/tablichka/Launcher.java
+++ Launcher/main/java/org/tablichka/Launcher.java
@@ -6,11 +6,11 @@ public final class Launcher extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
+
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+
     }
 }
