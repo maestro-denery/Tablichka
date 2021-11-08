9f603bcc9be6050aa8458c291f54fb55b651b5ae
Denery <63610262+ExtraExtremeERA@users.noreply.github.com>
2021-11-08 15:22:12 +0300

    buildscript improvement

--- EntityAI/main/java/org/tablichka/AI.java
+++ EntityAI/main/java/org/tablichka/AI.java
@@ -1,7 +1,5 @@
 package org.tablichka;
 
-import org.bukkit.plugin.java.JavaPlugin;
-
 public final class AI {
 
 }
--- Launcher/main/java/org/tablichka/Launcher.java
+++ Launcher/main/java/org/tablichka/Launcher.java
@@ -1,16 +1,20 @@
 package org.tablichka;
 
 import org.bukkit.plugin.java.JavaPlugin;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public final class Launcher extends JavaPlugin {
+    public static final Logger logger = LoggerFactory.getLogger("Tablichka");
 
     @Override
     public void onEnable() {
+        logger.info("Starting Tablet...");
 
     }
 
     @Override
     public void onDisable() {
-
+        logger.info("Shutting Down Tablet...");
     }
 }
