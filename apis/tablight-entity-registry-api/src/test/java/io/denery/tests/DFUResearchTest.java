2147573d77381381e5328f89ea9d90e589bc02e5
Denery <63610262+ExtraExtremeERA@users.noreply.github.com>
2022-03-11 20:40:57 +0300

    Edit readme

--- apis/tablight-entity-registry-api/src/test/java/io/denery/tests/DFUResearchTest.java
+++ apis/tablight-entity-registry-api/src/test/java/io/denery/tests/DFUResearchTest.java
@@ -1,8 +1,15 @@
 package io.denery.tests;
 
+import com.mojang.datafixers.optics.Adapter;
 import com.mojang.datafixers.optics.Lens;
 import com.mojang.datafixers.optics.Optics;
+import com.mojang.datafixers.optics.Prism;
+import com.mojang.datafixers.optics.Traversal;
+import com.mojang.datafixers.util.Either;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.Optional;
 
 class DFUResearchTest {
 	
@@ -10,17 +17,30 @@ class DFUResearchTest {
 	void someResearch() {
 		Lens<DummyStructS, DummyStructT, String, Integer> lens = Optics.lens(DummyStructS::getS, (i, dummyS) -> {
 			DummyStructT dummyStructT = new DummyStructT();
-			dummyStructT.setI(Integer.valueOf(dummyS.getS()) + i);
+			dummyStructT.setI(Integer.valueOf(dummyS.getS()));
+			dummyStructT.setStrings(dummyS.getStrings());
 			return dummyStructT;
 		});
-		var dummySInstance = new DummyStructS();
-		dummySInstance.setS("321");
-		System.out.println("converted: " + lens.update(123, dummySInstance).getI()); // converted: 444
-		System.out.println("view: " + lens.view(dummySInstance)); // view: 321
+		Prism<Optional<String>, Optional<Integer>, String, Integer> prism = Optics.prism(
+				s -> s.<Either<Optional<Integer>, String>>map(Either::right)
+						.orElseGet(() -> Either.left(s.map(Integer::valueOf)) /* It will anyway be null, mot sure about this. */),
+				Optional::of
+		);
+		Adapter<DummyStructS, DummyStructT, String, Integer> adapter = Optics.adapter(
+				DummyStructS::getS,
+				integer -> {
+					DummyStructT dummyStructT = new DummyStructT();
+					dummyStructT.setI(integer);
+					return dummyStructT;
+				}
+		);
+
+		Traversal<DummyStructS, DummyStructT, String, Integer> traversal = Optics.toTraversal(lens);
 	}
 
 	public static final class DummyStructS {
 		private String s;
+		List<String> strings;
 
 		public String getS() {
 			return s;
@@ -29,10 +49,19 @@ class DFUResearchTest {
 		public void setS(String s) {
 			this.s = s;
 		}
+
+		public List<String> getStrings() {
+			return strings;
+		}
+
+		public void setStrings(List<String> strings) {
+			this.strings = strings;
+		}
 	}
 
 	public static final class DummyStructT {
 		private int i;
+		List<String> strings;
 
 		public int getI() {
 			return i;
@@ -40,6 +69,14 @@ class DFUResearchTest {
 
 		public void setI(int i) {
 			this.i = i;
+		}
+
+		public List<String> getStrings() {
+			return strings;
+		}
+
+		public void setStrings(List<String> strings) {
+			this.strings = strings;
 		}
 	}
 
